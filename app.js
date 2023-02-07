73a9444439b2ddcfd944e6a8b3eaa076619eca14
Andy Albrecht <aalbrecht33733@gmail.com>
2023-02-07 10:39:15 -0500

    Final comments and updated code

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // alert("General Assembly SON!!!");
 let myName = "Sam Fisher";
 let myAge = "unknown";
-let thinkAge = 49;
+let speculatedAge = 49;
 let homeTown = "Confidential";
 
 // In Earlier versions, it was standard for JS 
@@ -18,7 +18,7 @@ favFoods.push("fish tacos", "berry smoothies");
 console.log(favFoods);
 // .pop() will 'pop off' the End of array:
 favFoods.pop();
-// Adding New Favorite Recipe to beggiming of array!*
+// Adding New Favorite Recipe to beggining of array!*
 favFoods.unshift("Quesadilla!");
 console.log(favFoods);
 
@@ -38,7 +38,7 @@ const aboutMe = {
   age: 37,
   homes: ["Brooklyn, NY", "Adirondacks, NY"]  
 }
-console.log(aboutMe.homes);
+console.log(aboutMe.homes); // ['Brooklyn, NY', 'Adirondacks, NY']
 console.log(aboutMe.homes[1]); // "Adirondacks, NY"
 aboutMe.dreamJob = "Full Stack Engineer";
 console.log(aboutMe)
@@ -132,7 +132,7 @@ while (number < 11) {
 // *If you know exactly how many times the loop should
 // execute, use a 'for' loop for its precision.
 // *If you're not sure how many times the loop has to
-// run but you do know when it should stop, us 'while'
+// run but you do know when it should stop, use 'while'
 
 // Functions in JavaScript:
 function takeDamage() {
@@ -149,7 +149,8 @@ function sayHello() {
 }
 sayHello();
 
-//
+// Not very efficient: because what if we wanted an
+// ameicanoTotal? Dont Repeat Yourself.
 function latteTotal() {
   const price = 4.50;
   const salesTaxRate = .10;
@@ -240,7 +241,7 @@ const companyName = document.querySelector('#company-name');
 // select the element with the class of sales-pitch
 const salesPitch = document.querySelector('.sales-pitch');
 // select all paragraph tags
-const allParagraphTags = document.querySelectorAll();
+const allParagraphTags = document.querySelectorAll('p');
 // select all elements inside of the advertisement div
 const allAdvertisementContent = document.querySelectorAll();
 
@@ -271,10 +272,17 @@ const postsDiv = document.querySelector("#posts");
 // Finally, we use the appendChild method to add our newPost inside of the postsDiv
 postsDiv.appendChild(newPost);
 
+// Commonly used Event Listeners:
+// *onload  // When the page loads.
+// *onclick  // When a user clicks something.
+// *onmouseover  // When a user hovers their mouse over something.
+// *onfocus  // When a user puts the cursor on a form field.
+
 // addEventListener() method:
 function sayHello() {
     console.log("Hello!");
 }
+// clicked button will print a message to the console:
 document.querySelector('button').addEventListener('click', sayHello);
 
 // onclick method:
@@ -291,36 +299,152 @@ document.querySelector('#login').addEventListener('click', respondToLoginAttempt
 // addEventListener() is newer and not as universally 
 // supported (looking at you, Internet Explorer).
 
+// The Event Object:
+document.querySelector('#disappearing-button').onclick = function(e) {
+    console.log(e.target);
+    e.target.remove();
+}
 
-// CODE PEN:
+// When attaching functions as event listeners, you 
+// might want that function to use the element that 
+// was clicked. Maybe you want to grab the text that 
+// was clicked to store or update it.
+
+// When defining the function, you can add a parameter, 
+// typically called e. This parameter, known as the 
+// event object, is passed into the function when it’s 
+// triggered. It contains helpful details about the 
+// event that triggered the function, including which 
+// element triggered the function: e.target.
+
+
+// === CodePen: Functions Practice ===
 // PROBLEM 1
 // Write a function that accepts one parameter - a name - and logs "Hello I am _____"
 // Fill in the blank with the name given to the function
-
+function helloThere(name){
+  console.log("Hello I am " + name);
+}
+helloThere("Wendy");
 
 // PROBLEM 2
-// Modify this function to accept two parameters, representing two numbers you want to multiply.
+// Modify this function accept two parameters, representing which numbers you want to multiply.
 // Then, add a code block that will multiply the two parameters and return the output.
-// The code block below the function is meant to check your work for the arguments 2 and 2.
-function multiply(){
-  //your code here
+function multiply(number1, number2){
+  return number1 * number2
 }
-
 const four = multiply(2, 2);
 if(four === 4){
-  console.log("The multiply function works!");
+  console.log("Multiplying works!");
 }else{
-  console.log("Something's wrong with the multiply function!")
+  console.log("Something's wrong with the multiply function.")
+}
+
+// PROBLEM 3
+// Write a function that accepts an array as a parameter
+// The function should loop through the given array and console.log each element in the array
+function printArray(arr){
+  for(let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+  }
+}
+printArray(["gold", "silver", "bronze", "participation trophy"])
+
+// PROBLEM 4
+// Write a function that finds the maximum number in a given array
+// Loop through the array and keep track of the biggest number seen so far
+// Once the loop is over, return the biggest number in the array
+function findMax(arr){
+  let max = arr[0];
+  for(let i = 1; i < arr.length; i++){
+    if(arr[i] > max){
+      max = arr[i];
+    }
+  }
+  return max;
 }
 
 
+// === CodePen: DOM Manipulation ===
+// Reading information from DOM elements
+// 1) Console.log the text of the element with the id of "title"
+const titleText = document.getElementById("title").innerText;
+console.log(titleText);
+// 2) Console.log the class of the first span on the page
+const firstSpan = document.querySelector("span");
+console.log(firstSpan.getAttribute("class"));
+
+// Updating properties of DOM elements
+// 1) Turn the text blue in the first element with the "blue" class
+const blueSpan = document.querySelector(".blue");
+blueSpan.style.color = "blue"
+// 2) Change the text of the "#title" element to a greeting of your choice
+document.querySelector("#title").innerText = "Come on in!";
+// Creating new DOM elements
+// 1) Create an h4 element with the text of "Powered by Javascript"
+const h4 = document.createElement("h4");
+h4.innerText = "Powered by JavaScript"
+// 1b) Add that element to the end of the page
+document.body.append(h4);
+// 2) Create a ul with class "todos" and add it to the end of the page
+const toDosUL = document.createElement('ul');
+toDosUL.setAttribute("class", "todos");
+document.body.appendChild(toDosUL);
+// 2b) Looping through this array, add the strings as <li> elements
+const toDoList = ["create elements", "update them", "add them to DOM"];
+for(let i = 0; i < toDoList.length; i++){
+  console.log(toDoList[i]);
+  const newListItem = document.createElement("li");
+  newListItem.innerText = toDoList[i];
+  toDosUL.appendChild(newListItem);
+}
+
+// Removing elements from the DOM
+// 1) Remove the #title h1 from the page
+document.getElementById('title').remove();
+// 2) Using a while loop, remove each child of the ul until it is empty
+const toDoChildren = document.querySelector('.todos').childNodes;
+while(toDoChildren.length > 0){
+  toDoChildren[0].remove();
+}
+
+
+// === CodePen: Practice using Event Listeners ===
+// PROBLEM 1
+// Add an event listener to the button with the generate-greeting id
+// When the button is clicked, trigger the sayHello function
+function sayHello(){
+  console.log("hello!");
+}
+document.querySelector('#generate-greeting').addEventListener('click', sayHello);
+
+// PROBLEM 2
+// When the user clicks the "Add to the List!" button:
+// 1) Add a new li to the .todos ul
+// 2) Fill in the li with the text from the input field
+// 3) Reset the value of the input field to be empty
+function addToDo(){
+  const text = document.querySelector("#new-todo").value;
+  const newItem = document.createElement('li');
+  newItem.innerText = text;
+  document.querySelector('.todos').appendChild(newItem);
+  document.getElementById("new-todo").value = null;
+}
+document.querySelector("#generate-todo").addEventListener('click', addToDo)
+
 // PROBLEM 3
-// Write a function that accepts an array as a parameter.
-// The function should loop through the given array and console.log each element in the array.
-// Create your own array to test your work.
-
-
-// PROBLEM 4
-// Write a function that finds the maximum number in a given array.
-// Loop through the array and keep track of the biggest number seen so far.
-// Once the loop is over, return the biggest number in the array.
+// When the user clicks any of the li items you've added, cross it out (add styling to create the line through effect)
+// This will involve attaching a listener to each <li> as you add them
+// You will have to modify the code you've written above!
+// Updated answer from above:
+function addToDo(){
+  const text = document.querySelector("#new-todo").value;
+  const newItem = document.createElement('li');
+  newItem.innerText = text;
+  newItem.onclick = function(e){
+    e.target.style["text-decoration"] = "line-through";
+  }
+  document.querySelector('.todos').appendChild(newItem);
+  document.querySelector("#new-todo").value = null;
+}
+document.querySelector("#generate-todo").addEventListener('click', addToDo)
